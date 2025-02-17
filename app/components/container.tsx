f6271ae064bd7bde5ea0cad3448628cce5fac012
maestro194 <dongboy1904@gmail.com>
2025-02-17 18:29:19 +0700

    up

--- app/components/container.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function () {
-  return (
-    <div className='bg-slate-300 w-full lg:w-[1080px] lg:p-5 min-h-screen'>
-        App Container
-    </div>
-  )
-}
